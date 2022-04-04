f61bcc68022baf9ff08abfe958591cf0e29bd579
novi <novilusio@protonmail.com>
2022-04-04 15:25:21 +0300

    Task2

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,7 +1,7 @@
 import { storage, Context } from "near-sdk-as"
 
 // return the string 'hello world'
-export function helloWorld(): string {
+export function helloWorld(name: string): string {
   return 'hello ' + name
 }
 
